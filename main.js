ef1f11ec82f414cd4e056ebd76ffba199543b5fd
phuocnguyn <huynhphuocnguyen2412@gmail.com>
2023-11-19 04:04:42 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,107 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const header_song = $(".header-name-song");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const btn_toggle_play = $(".btn-toggle-play");
+const btn_next = $(".btn-next");
+const btn_prev = $(".btn-prev");
+const btn_play = $(".btn-play");
+const btn_pause = $(".btn-pause ");
+const btn_repeat = $(".btn-repeat");
+const btn_ramdom = $(".btn-ramdom");
+const progress = $("#progress");
+
+const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+    duration: 100000,
+    iterations: Infinity,
+});
+
+cdThumbAnimate.pause();
+fetch("http://localhost:3000/songs")
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (songsArray) {
+        const songList = songsArray.map(function (song) {
+            return `
+            <div class="song">
+                <div
+                    class="thumb"
+                    style="
+                        background-image: url('${song.image}');
+                    "
+                ></div>
+                <div class="body">
+                    <h3 class="title">${song.name}</h3>
+                    <p class="author">${song.singer}</p>
+                </div>
+                <div class="option">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+            </div>
+            `;
+        });
+        $(".playlist").innerHTML = songList.join("");
+        return songsArray;
+    })
+    .then(function (songsArray) {
+        header_song.innerHTML = songsArray[0].name;
+        cdThumb.style.backgroundImage = `url(${songsArray[0].image})`;
+        audio.src = "./audio/" + songsArray[0].path;
+        return songsArray;
+    })
+
+    .then(function (songsArray) {
+        let currentIndexSong = songsArray.findIndex(function (song) {
+            return song.name === header_song.innerText;
+        });
+
+        function playSong(index) {
+            header_song.innerHTML = songsArray[index].name;
+            cdThumb.style.backgroundImage = `url(${songsArray[index].image})`;
+            audio.src = "./audio/" + songsArray[index].path;
+            audio.play();
+        }
+        audio.ontimeupdate = function () {
+            progress.value = Math.floor(
+                (audio.currentTime / audio.duration) * 100
+            );
+        };
+        btn_toggle_play.addEventListener("click", function () {
+            if (audio.paused) {
+                audio.play();
+                $(".icon-play").style.display = "none";
+                $(".icon-pause").style.display = "inline-block";
+                cdThumbAnimate.play();
+            } else {
+                audio.pause();
+                $(".icon-pause").style.display = "none";
+                $(".icon-play").style.display = "inline-block";
+                cdThumbAnimate.pause();
+            }
+        });
+        btn_next.addEventListener("click", function () {
+            if (audio.currentTime < audio.duration) {
+                audio.currentTime += 5;
+            } else {
+                audio.currentTime = 0;
+            }
+        });
+        progress.onchaged = function () {
+            audio.currentTime = (progress.value / 100) * audio.duration;
+        };
+        btn_repeat.addEventListener("click", function () {
+            audio.load();
+            audio.play();
+        });
+        btn_prev.addEventListener("click", function () {
+            if (currentIndexSong == 0) playSong(currentIndexSong);
+            else playSong(--currentIndexSong);
+        });
+        btn_next.addEventListener("click", function () {
+            if (currentIndexSong == songsArray.length - 1) playSong(0);
+            else playSong(++currentIndexSong);
+        });
+    });
